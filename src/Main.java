c9aa0bfde936df0321413ad3185fdf15a3810db0
Antonina Yurchenko <tonya2512@yandex.ru>
2023-09-02 10:15:11 +0300

    .gitignore

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticket = 10860;
+        int mile = 20;
+        String name = "Количество бонусных баллов";
+        System.out.println(name + ticket / mile);
+
+    }
+}
